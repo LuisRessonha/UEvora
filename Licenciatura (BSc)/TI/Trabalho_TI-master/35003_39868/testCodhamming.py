c5dd1f0aa8055e5673fb8ee0df3bb86cf7ec6ff0
LuisRessonha <luisressonha96@gmail.com>
2021-03-05 16:03:34 +0000

    .

---
+++ Licenciatura (BSc)/TI/Trabalho_TI-master/35003_39868/testCodhamming.py
@@ -0,0 +1,5 @@
+import hamming_7_4
+
+initMsg = ""
+codMesg = hamming_7_4.cod_hamming(initMsg)
+print(codMesg)
---
+++ Licenciatura (BSc)/TI/Trabalho_TI-master/35003_39868/testDecodhamming.py
@@ -0,0 +1,5 @@
+import hamming_7_4
+
+mensagem = input()
+ham = hamming_7_4.decod_hamming(mensagem)
+print(ham)
